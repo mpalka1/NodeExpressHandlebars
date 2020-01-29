a47203b6e599959ae900c4a90eb6a53d00ef9e4a
Matthew Palka <palkamatthew@gmail.com>
2020-01-28 21:48:18 -0600

    add new table

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-      orm.all("burgers", function(res) {
+      orm.all("ydzp0706rt009w1c.burgers", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: function(name, cb) {
-      orm.create("burgers", ["burger_name","eaten"],[name,false], function(res) {
+      orm.create("ydzp0706rt009w1c.burgers", ["burger_name","eaten"],[name,false], function(res) {
         cb(res);
       });
     },
       update: function(id,cb) {
         var condition = "id ="+id;
-        orm.update("burgers",{eaten:true},condition,cb)
+        orm.update("ydzp0706rt009w1c.burgers",{eaten:true},condition,cb)
     },
       delete: function(id,cb) {
         var condition = "id ="+id;
-        orm.delete("burgers", condition,cb)
+        orm.delete("ydzp0706rt009w1c.burgers", condition,cb)
     }
   };
   
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3030;
 
 var app = express();
 
