39d4b29efd76fdadc7e7fbbf3b87230b19e66b17
Matthew Palka <palkamatthew@gmail.com>
2020-01-28 22:01:47 -0600

    123

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-      orm.all("ydzp0706rt009w1c.burgers", function(res) {
+      orm.all("burgers", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: function(name, cb) {
-      orm.create("ydzp0706rt009w1c.burgers", ["burger_name","eaten"],[name,false], function(res) {
+      orm.create("burgers", ["burger_name","eaten"],[name,false], function(res) {
         cb(res);
       });
     },
       update: function(id,cb) {
         var condition = "id ="+id;
-        orm.update("ydzp0706rt009w1c.burgers",{eaten:true},condition,cb)
+        orm.update("burgers",{eaten:true},condition,cb)
     },
       delete: function(id,cb) {
         var condition = "id ="+id;
-        orm.delete("ydzp0706rt009w1c.burgers", condition,cb)
+        orm.delete("burgers", condition,cb)
     }
   };
   
